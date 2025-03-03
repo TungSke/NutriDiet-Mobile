bf5d345924b66182b144778b617471c8a21e7c63
Baolngse171848 <giabaole123456789@gmail.com>
2025-03-03 17:28:31 +0700

    fix UI add meal at home UI

--- lib/components/foodList_component_widget.dart
+++ lib/components/foodList_component_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+// Import màn hình FoodDetail
 import '../services/food_service.dart';
 import '../services/models/food.dart';
 
@@ -27,9 +28,6 @@ class _FoodListComponentWidgetState extends State<FoodListComponentWidget> {
   Future<void> fetchFoods() async {
     try {
       _foodList = _foodService.getAllFoods(pageIndex: 1, pageSize: 10);
-      print("Fetching food data...");
-      final foods = await _foodList;
-      print("Data received: ${foods.map((f) => f.toJson()).toList()}");
     } catch (e) {
       print("Error fetching food data: $e");
     }
@@ -41,129 +39,133 @@ class _FoodListComponentWidgetState extends State<FoodListComponentWidget> {
     return FutureBuilder<List<Food>>(
       future: _foodList,
       builder: (context, snapshot) {
-        print("FutureBuilder state: ${snapshot.connectionState}");
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         }
         if (snapshot.hasError) {
-          print("Error snapshot: ${snapshot.error}");
           return Center(child: Text('Lỗi: ${snapshot.error}'));
         }
         if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          print("No data available");
-          return Center(child: Text('Không có dữ liệu'));
+          return const Center(child: Text('Không có dữ liệu'));
         }
 
         return Padding(
           padding: const EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 16.0),
           child: ListView.builder(
             shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
+            physics: const NeverScrollableScrollPhysics(),
             itemCount: snapshot.data!.length,
             itemBuilder: (context, index) {
               final food = snapshot.data![index];
-              return Container(
-                width: double.infinity,
-                height: 104.0,
-                margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-                decoration: BoxDecoration(
-                  color: FlutterFlowTheme.of(context).lightGrey,
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(16.0),
-                        child: food.imageUrl != null
-                            ? Image.network(
-                                food.imageUrl!,
-                                width: 96.0,
-                                height: 96.0,
-                                fit: BoxFit.cover,
-                              )
-                            : Image.asset(
-                                'assets/images/placeholder.png',
-                                width: 96.0,
-                                height: 96.0,
-                                fit: BoxFit.cover,
-                              ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              food.foodName,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: FlutterFlowTheme.of(context)
-                                  .bodyMedium
-                                  .override(
-                                    fontFamily: 'figtree',
-                                    fontSize: 18.0,
-                                    letterSpacing: 0.0,
-                                    fontWeight: FontWeight.w500,
-                                    useGoogleFonts: false,
-                                  ),
-                            ),
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 4.0),
-                                  child: SvgPicture.asset(
-                                    'assets/images/fire-icon--1.svg',
-                                    width: 24.0,
-                                    height: 24.0,
-                                    fit: BoxFit.cover,
-                                  ),
+
+              return GestureDetector(
+                onTap: () {
+                  context.pushNamed('brek_fast_iIngredients');
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 104.0,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 4.0, horizontal: 8.0),
+                  decoration: BoxDecoration(
+                    color: FlutterFlowTheme.of(context).lightGrey,
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(16.0),
+                          child: food.imageUrl != null
+                              ? Image.network(
+                                  food.imageUrl!,
+                                  width: 96.0,
+                                  height: 96.0,
+                                  fit: BoxFit.cover,
+                                )
+                              : Image.asset(
+                                  'assets/images/placeholder.png',
+                                  width: 96.0,
+                                  height: 96.0,
+                                  fit: BoxFit.cover,
                                 ),
-                                Expanded(
-                                  child: Text(
-                                    '${food.calories ?? '0'} Calories',
-                                    style: FlutterFlowTheme.of(context)
-                                        .bodyMedium
-                                        .override(
-                                          fontFamily: 'figtree',
-                                          color:
-                                              FlutterFlowTheme.of(context).grey,
-                                          fontSize: 16.0,
-                                          letterSpacing: 0.0,
-                                          fontWeight: FontWeight.normal,
-                                          useGoogleFonts: false,
-                                        ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 4.0),
-                                  child: Text("Khẩu phần:"),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    food.servingSize ?? 'Không rõ',
-                                    style: FlutterFlowTheme.of(context)
-                                        .bodyMedium
-                                        .override(
-                                          fontFamily: 'figtree',
-                                          color:
-                                              FlutterFlowTheme.of(context).grey,
-                                          fontSize: 16.0,
-                                          letterSpacing: 0.0,
-                                          fontWeight: FontWeight.normal,
-                                          useGoogleFonts: false,
-                                        ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ].divide(const SizedBox(height: 4.0)),
                         ),
-                      ),
-                    ].divide(const SizedBox(width: 12.0)),
+                        Expanded(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                food.foodName,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'figtree',
+                                      fontSize: 18.0,
+                                      letterSpacing: 0.0,
+                                      fontWeight: FontWeight.w500,
+                                      useGoogleFonts: false,
+                                    ),
+                              ),
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 4.0),
+                                    child: SvgPicture.asset(
+                                      'assets/images/fire-icon--1.svg',
+                                      width: 24.0,
+                                      height: 24.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      '${food.calories ?? '0'} Calories',
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyMedium
+                                          .override(
+                                            fontFamily: 'figtree',
+                                            color: FlutterFlowTheme.of(context)
+                                                .grey,
+                                            fontSize: 16.0,
+                                            letterSpacing: 0.0,
+                                            fontWeight: FontWeight.normal,
+                                            useGoogleFonts: false,
+                                          ),
+                                    ),
+                                  ),
+                                  const Padding(
+                                    padding: EdgeInsets.only(right: 4.0),
+                                    child: Text("Khẩu phần:"),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      food.servingSize ?? 'Không rõ',
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyMedium
+                                          .override(
+                                            fontFamily: 'figtree',
+                                            color: FlutterFlowTheme.of(context)
+                                                .grey,
+                                            fontSize: 16.0,
+                                            letterSpacing: 0.0,
+                                            fontWeight: FontWeight.normal,
+                                            useGoogleFonts: false,
+                                          ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ].divide(const SizedBox(height: 4.0)),
+                          ),
+                        ),
+                      ].divide(const SizedBox(width: 12.0)),
+                    ),
                   ),
                 ),
               );
--- lib/components/food_screen_widget.dart
+++ lib/components/food_screen_widget.dart
@@ -1,6 +1,7 @@
 import 'package:diet_plan_app/components/food_screen_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:provider/provider.dart';
 
 import '/components/foodList_component_widget.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
@@ -19,6 +20,7 @@ class _FoodScreenWidgetState extends State<FoodScreenWidget>
   late FoodScreenModel _model;
 
   final animationsMap = <String, AnimationInfo>{};
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void setState(VoidCallback callback) {
@@ -59,186 +61,110 @@ class _FoodScreenWidgetState extends State<FoodScreenWidget>
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Container(
-          height: 119.0,
-          decoration: const BoxDecoration(),
-          child: Align(
-            alignment: const AlignmentDirectional(0.0, 1.0),
-            child: Padding(
-              padding:
-                  const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 16.0),
-              child: Text(
-                'Thực phẩm',
-                maxLines: 1,
-                style: FlutterFlowTheme.of(context).bodyMedium.override(
-                      fontFamily: 'figtree',
-                      fontSize: 22.0,
-                      letterSpacing: 0.0,
-                      fontWeight: FontWeight.bold,
-                      useGoogleFonts: false,
-                      lineHeight: 1.5,
-                    ),
-              ).animateOnPageLoad(animationsMap['textOnPageLoadAnimation']!),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 8.0),
-          child: TextFormField(
-            controller: _model.textController,
-            focusNode: _model.textFieldFocusNode,
-            onFieldSubmitted: (_) async {
-              context.pushNamed('Search_result_screen');
-            },
-            autofocus: false,
-            obscureText: false,
-            decoration: InputDecoration(
-              labelStyle: FlutterFlowTheme.of(context).labelMedium.override(
-                    fontFamily: 'figtree',
-                    color: FlutterFlowTheme.of(context).grey,
-                    letterSpacing: 0.0,
-                    useGoogleFonts: false,
-                  ),
-              hintText: 'Tìm kiếm',
-              hintStyle: FlutterFlowTheme.of(context).labelMedium.override(
-                    fontFamily: 'figtree',
-                    color: FlutterFlowTheme.of(context).grey,
-                    letterSpacing: 0.0,
-                    useGoogleFonts: false,
-                  ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: Color(0x00000000),
-                  width: 1.0,
-                ),
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: Color(0x00000000),
-                  width: 1.0,
-                ),
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: FlutterFlowTheme.of(context).error,
-                  width: 1.0,
-                ),
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-              focusedErrorBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: FlutterFlowTheme.of(context).error,
-                  width: 1.0,
-                ),
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-              filled: true,
-              fillColor: FlutterFlowTheme.of(context).lightGrey,
-              prefixIcon: const Icon(
-                Icons.search_sharp,
-              ),
-            ),
-            style: FlutterFlowTheme.of(context).bodyMedium.override(
-                  fontFamily: 'figtree',
-                  letterSpacing: 0.0,
-                  useGoogleFonts: false,
-                ),
-            cursorColor: FlutterFlowTheme.of(context).primaryText,
-            validator: _model.textControllerValidator.asValidator(context),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 0.0),
-            child: ListView(
-              padding: const EdgeInsets.fromLTRB(
-                0,
-                8.0,
-                0,
-                16.0,
-              ),
-              scrollDirection: Axis.vertical,
+    context.watch<FFAppState>();
+    return GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+            key: scaffoldKey,
+            body: ListView(
               children: [
-                InkWell(
-                  splashColor: Colors.transparent,
-                  focusColor: Colors.transparent,
-                  hoverColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  onTap: () async {
-                    context.pushNamed('brek_fast_iIngredients');
-                  },
-                  child: wrapWithModel(
-                    model: _model.foodListComponentModel1,
-                    updateCallback: () => safeSetState(() {}),
-                    child: const FoodListComponentWidget(),
+                Container(
+                  height: 119.0,
+                  decoration: const BoxDecoration(),
+                  child: Align(
+                    alignment: const AlignmentDirectional(0.0, 1.0),
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(
+                          0.0, 0.0, 0.0, 16.0),
+                      child: Text(
+                        'Thực phẩm',
+                        maxLines: 1,
+                        style: FlutterFlowTheme.of(context).bodyMedium.override(
+                              fontFamily: 'figtree',
+                              fontSize: 22.0,
+                              letterSpacing: 0.0,
+                              fontWeight: FontWeight.bold,
+                              useGoogleFonts: false,
+                              lineHeight: 1.5,
+                            ),
+                      ).animateOnPageLoad(
+                          animationsMap['textOnPageLoadAnimation']!),
+                    ),
                   ),
                 ),
-                InkWell(
-                  splashColor: Colors.transparent,
-                  focusColor: Colors.transparent,
-                  hoverColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  onTap: () async {
-                    context.pushNamed('brek_fast_iIngredients');
-                  },
-                  child: wrapWithModel(
-                    model: _model.foodListComponentModel2,
-                    updateCallback: () => safeSetState(() {}),
-                    child: const FoodListComponentWidget(),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(
+                      20.0, 0.0, 20.0, 8.0),
+                  child: TextFormField(
+                    controller: _model.textController,
+                    focusNode: _model.textFieldFocusNode,
+                    onFieldSubmitted: (_) async {
+                      context.pushNamed('Search_result_screen');
+                    },
+                    autofocus: false,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      labelStyle:
+                          FlutterFlowTheme.of(context).labelMedium.override(
+                                fontFamily: 'figtree',
+                                color: FlutterFlowTheme.of(context).grey,
+                                letterSpacing: 0.0,
+                                useGoogleFonts: false,
+                              ),
+                      hintText: 'Tìm kiếm',
+                      hintStyle:
+                          FlutterFlowTheme.of(context).labelMedium.override(
+                                fontFamily: 'figtree',
+                                color: FlutterFlowTheme.of(context).grey,
+                                letterSpacing: 0.0,
+                                useGoogleFonts: false,
+                              ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Color(0x00000000),
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Color(0x00000000),
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: FlutterFlowTheme.of(context).error,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: FlutterFlowTheme.of(context).error,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      filled: true,
+                      fillColor: FlutterFlowTheme.of(context).lightGrey,
+                      prefixIcon: const Icon(
+                        Icons.search_sharp,
+                      ),
+                    ),
+                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                          fontFamily: 'figtree',
+                          letterSpacing: 0.0,
+                          useGoogleFonts: false,
+                        ),
+                    cursorColor: FlutterFlowTheme.of(context).primaryText,
+                    validator:
+                        _model.textControllerValidator.asValidator(context),
                   ),
                 ),
-                InkWell(
-                  splashColor: Colors.transparent,
-                  focusColor: Colors.transparent,
-                  hoverColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  onTap: () async {
-                    context.pushNamed('brek_fast_iIngredients');
-                  },
-                  child: wrapWithModel(
-                    model: _model.foodListComponentModel3,
-                    updateCallback: () => safeSetState(() {}),
-                    child: const FoodListComponentWidget(),
-                  ),
-                ),
-                InkWell(
-                  splashColor: Colors.transparent,
-                  focusColor: Colors.transparent,
-                  hoverColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  onTap: () async {
-                    context.pushNamed('brek_fast_iIngredients');
-                  },
-                  child: wrapWithModel(
-                    model: _model.foodListComponentModel4,
-                    updateCallback: () => safeSetState(() {}),
-                    child: const FoodListComponentWidget(),
-                  ),
-                ),
-                InkWell(
-                  splashColor: Colors.transparent,
-                  focusColor: Colors.transparent,
-                  hoverColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  onTap: () async {
-                    context.pushNamed('brek_fast_iIngredients');
-                  },
-                  child: wrapWithModel(
-                    model: _model.foodListComponentModel5,
-                    updateCallback: () => safeSetState(() {}),
-                    child: const FoodListComponentWidget(),
-                  ),
-                ),
-              ].divide(const SizedBox(height: 16.0)),
-            ),
-          ),
-        ),
-      ],
-    );
+                const FoodListComponentWidget(),
+              ],
+            )));
   }
 }
