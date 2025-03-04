605cb5ef0aa13352480a9f193534e6f19579f74b
Minh Mạnh <tivip1216vn@gmail.com>
2025-03-04 20:02:30 +0700

    login gg feature

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -24,7 +27,7 @@ android {
         applicationId = "com.example.diet_plan_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
