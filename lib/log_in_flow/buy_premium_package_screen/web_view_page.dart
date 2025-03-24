26d0a64f0d02aaf768917b4b4655b479be2e824f
Baolngse171848 <giabaole123456789@gmail.com>
2025-03-24 22:31:43 +0700

    add webview

---
+++ lib/log_in_flow/buy_premium_package_screen/web_view_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatelessWidget {
+  const WebViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://google.com'));
+    return Scaffold(
+      appBar: AppBar(),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
