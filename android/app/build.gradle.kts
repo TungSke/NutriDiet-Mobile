c3a31284d66098786823878e4bdbf46c74ea8848
tungske <trinhsontung2410@gmail.com>
2025-04-28 09:00:32 +0700

    change sdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,8 @@ android {
         applicationId = "com.example.diet_plan_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 30
+        minSdk = 26
+        targetSdk = 34
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
